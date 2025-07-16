ebc3d50b67cec23957d1ef0eee78064237840457
Marcus Bondezan <marcus.bondezan@gmail.com>
2025-07-16 14:46:50 -0300

    upgrading github runner node version and using fetch lib

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -1,8 +1,5 @@
-const request = require("request-promise");
-
 const options = {
   method: "GET",
-  uri: "https://icanhazdadjoke.com/",
   headers: {
     Accept: "application/json",
     "User-Agent": "Writing JavaScript action GitHub Skills course.",
@@ -11,8 +8,9 @@ const options = {
 };
 
 async function getJoke() {
-  const res = await request(options);
-  return res.joke;
+  const response = await fetch("https://icanhazdadjoke.com/", options);
+  const data = await response.json();
+  return data.joke;
 }
 
 module.exports = getJoke;
